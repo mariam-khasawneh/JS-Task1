a270cef5803f64e7ea09e385ae621e85f9cc6028
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-06-03 22:09:38 +0300

    adding q2

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//Q2
+// reading number using propmt
+let number = parseInt(prompt(`Please enter number`));
+if (typeof (number) == "number") {
+    alert(`The number you entered was ${number}`);
+} else{ 
+    alert(`Please Enter a valid number`);
+}
+//__________________________________________________________________________________________________
+
+
+
+
