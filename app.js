9980a3048c1facf177ee886d68abe68459c99554
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-06-03 22:11:02 +0300

    adding q3

--- app.js
+++ app.js
@@ -10,5 +10,30 @@ if (typeof (number) == "number") {
 //__________________________________________________________________________________________________
 
 
+//Q3
+// Read two numbers and display in ascending order
+let num1 = parseInt(prompt(`Please enter first number to display in ascending order`));
+let num2 = parseInt(prompt(`Please enter secound number  to display in ascending order`));
+let largest;
+let smallest;
+
+//condition statment => ascending order
+if (num1 > num2) {
+    largest = num1;
+    smallest = num2;
+    alert(`Your number in ascending order : 
+    ${smallest}
+    ${largest}`);
+} else if (num1 < num2) {
+    smallest = num1;
+    largest = num2;
+    alert(`Your number in ascending order : 
+    ${smallest}
+    ${largest}`);
+} else if (num1 == num2){
+    alert(`The two numbers are equal`);
+ }
+//_________________________________________________________________________________________________
+
 
 
